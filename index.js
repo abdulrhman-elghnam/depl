2708acb6e969a10b0fd2d35183c26112298f02b6
Abdulrhman El-ghnam <abdulrhman.mohamed.atya@gmail.com>
2026-09-01 01:46:32 +0300

    feat: add getData function and user endpoint to retrieve user data

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import express from "express"
 
 const app =  express()
 
+  getData = async (params) => {
+    setTimeout(()=>{
+        return { 
+            name : "ahmed",
+            age : 24,
+        }
+    },5000)
+}
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
@@ -9,4 +17,10 @@ app.listen(3000,()=>{
 
 app.get("/",( req , res  , next )=>{
     res.status(200).json({msg:"hi from backed side "})
+})
+
+
+app.get("/user", async ( req , res  , next )=>{
+    const serviceFeedback = await getData()
+    res.status(200).json({msg:"done" , data :serviceFeedback })
 })
