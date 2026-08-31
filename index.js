e418cf4100ab2a2e6902d59d16b473e8230e690d
Abdulrhman El-ghnam <abdulrhman.mohamed.atya@gmail.com>
2026-09-01 01:49:32 +0300

    style: format code for consistency and readability

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 import express from "express"
 
-const app =  express()
+const app = express()
 
-  getData = async (params) => {
-    setTimeout(()=>{
-        return { 
-            name : "ahmed",
-            age : 24,
+const getData = async () => {
+    setTimeout(() => {
+        return {
+            name: "ahmed",
+            age: 24,
         }
-    },450)
+    }, 450)
 }
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server is running on port 3000");
 })
 
-app.get("/",( req , res  , next )=>{
-    res.status(200).json({msg:"hi from backed side "})
+app.get("/", (req, res, next) => {
+    res.status(200).json({ msg: "hi from backed side " })
 })
 
 
-app.get("/user", async ( req , res  , next )=>{
+app.get("/user", async (req, res, next) => {
     const serviceFeedback = await getData()
-    res.status(200).json({msg:"done" , data :serviceFeedback })
+    res.status(200).json({ msg: "done", data: serviceFeedback })
 })
