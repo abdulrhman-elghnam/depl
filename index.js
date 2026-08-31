41df81617e89d585ff867acc25543f6441fc0cc9
Abdulrhman El-ghnam <abdulrhman.mohamed.atya@gmail.com>
2026-09-01 02:25:53 +0300

    feat: add URL and URLSearchParams implementations and utility functions
    - Introduced `urlencoded.js` for parsing and serializing URL-encoded data.
    - Added `utils.js` for various utility functions including object handling and property definitions.
    - Created `package.json` for the whatwg-url package with dependencies and scripts.
    - Implemented `webidl2js-wrapper.js` to export URL and URLSearchParams.

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 import express from "express"
+import mongo from "mongodb"
 
 const app = express()
+app.use(express.json());
 
 const getData = async () => {
     return {
         name : "ahmed"
     }
-
 }
 
-app.listen(3000, () => {
+
+const connection = new  mongo.MongoClient()
+
+
+
+app.listen(3000, async () => {
+    await connection.connect().then(()=>{
+        console.log("connected ");
+    });
     console.log("server is running on port 3000");
 })
+
+const databaseName = connection.db("test");
 
 app.get("/", (req, res, next) => {
     res.status(200).json({ msg: "hi from backed side " })
 })
 
 
-app.get("/user", async (req, res, next) => {
 
-    const serviceFeedback = await getData()
+app.post("/user", async (req, res, next) => {
+   try {
+     const serviceFeedback = await databaseName.collection("user").insertOne(req.body)
+    console.log(serviceFeedback);
+    return res.status(200).json({ msg: "inserted", data: serviceFeedback })
+   } catch (error) {
+    console.log(error);
+   }
+})
+
+app.get("/user", async (req, res, next) => {
+    const serviceFeedback = await databaseName.collection("user").find()
     console.log(serviceFeedback);
     return res.status(200).json({ msg: "done", data: serviceFeedback })
 })
