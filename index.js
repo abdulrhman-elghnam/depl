f3bca15de2bbd0b6d338606179ebbd2d04589424
Abdulrhman El-ghnam <abdulrhman.mohamed.atya@gmail.com>
2026-09-01 01:39:01 +0300

    feat: initialize project with package.json and Vercel configuration
    - Added package.json to manage project dependencies and scripts
    - Included express as a dependency
    - Configured Vercel with rewrites to route all requests to index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app =  express()
+
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
+
+app.get("/",( req , res  , next )=>{
+    res.status(200).json({msg:"hi from backed side "})
+})
